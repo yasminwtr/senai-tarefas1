87dd2fe93c1c60d634a2c3b0d8bbdc1c92bc77d4
Yasmin <yasmintitow@gmail.com>
2021-11-30 17:15:00 -0300

    novas tarefas

---
+++ aula lucas/petshop/script.js
@@ -0,0 +1,17 @@
+let nomeAnimal = ''
+let tipoAnimal = ''
+let tamAnimal = ''
+const valorGato = 30
+const valorCao = 40
+
+let valorTotal = ''
+
+nomeAnimal = prompt("Qual é o nome do seu animalzinho?")
+tipoAnimal = prompt("Ele é gato ou cão?")
+tamAnimal = prompt("Qual é o porte do seu animalzinho? (P/M/G)")
+
+    switch(true){
+        case tipoAnimal && 
+    }
+
+alert("O banho e tosa do ${tipoAnimal) ${nomeAnimal} de porte ${tamAnimal} tem como valor total ${valorTotal},00")

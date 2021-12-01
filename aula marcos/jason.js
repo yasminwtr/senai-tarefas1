48c4030e8c80a459cb3b73b821559a6c38d6374e
Yasmin <yasmintitow@gmail.com>
2021-12-01 17:11:13 -0300

    aula marcos

---
+++ aula marcos/jason.js
@@ -0,0 +1,9 @@
+const mochila = {
+    nome: "mochila de pano",
+    idadeAnos: 2,
+    tamanhoCm: 40,
+    cor: "vermelho"
+}
+
+console.log(mochila["idadeAnos"]);
+console.log(mochila.idadeAnos);
---
+++ aula marcos/yasmin.js
@@ -0,0 +1,8 @@
+const yasmin = {
+    nome: "yasmin",
+    idade: 17,
+    tamanho: 1.60
+}
+
+console.log(yasmin["idade"]);
+console.log(yasmin.idade);
